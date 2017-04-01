4024436e9b4d8ba320b6847ab67e7ebc38f69871
Shao Cheng <astrohavoc@gmail.com>
2017-04-01 11:49:54 +0800

    Implement wrapper for handling errors on Windows.

--- src/Foreign/Easy.hs
+++ src/Foreign/Easy.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# LANGUAGE CApiFFI #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE InterruptibleFFI #-}
 {-# LANGUAGE Strict #-}
@@ -11,11 +12,16 @@ module Foreign.Easy
     , findSymbol
     ) where
 
-import Control.Exception (Exception)
+import Control.Exception (Exception, throwIO, bracket)
+import Control.Monad (unless)
+import Data.Bits ((.|.))
+import Data.Functor (void)
 import Data.Word (Word32)
-import Foreign.C.String (CString)
+import Foreign.C.String (CString, withCString, peekCWString)
 import Foreign.C.Types (CInt(..), CChar, CWchar)
-import Foreign.Ptr (Ptr)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Ptr (Ptr, nullPtr)
+import Foreign.Storable (peek)
 
 newtype Module = Module
     { getModule :: Ptr ()
@@ -47,17 +53,22 @@ type LPCWSTR = Ptr CWchar
 
 type LPWSTR = Ptr CWchar
 
-type TCHAR = CWchar
-
 type WINBOOL = Bool
 
-foreign import ccall interruptible "LoadModuleExW" c_LoadModuleExW
-               :: LPCWSTR -> HANDLE -> DWORD -> IO HMODULE
+foreign import capi "windows.h value FORMAT_MESSAGE_FROM_SYSTEM"
+               c_FORMAT_MESSAGE_FROM_SYSTEM :: DWORD
+
+foreign import capi
+               "windows.h value FORMAT_MESSAGE_ALLOCATE_BUFFER"
+               c_FORMAT_MESSAGE_ALLOCATE_BUFFER :: DWORD
+
+foreign import ccall interruptible "LoadLibraryExW"
+               c_LoadLibraryExW :: LPCWSTR -> HANDLE -> DWORD -> IO HMODULE
 
 foreign import ccall interruptible "GetProcAddress"
                c_GetProcAddress :: HMODULE -> LPCSTR -> IO FARPROC
 
-foreign import ccall interruptible "FreeModule" c_FreeModule ::
+foreign import ccall interruptible "FreeLibrary" c_FreeLibrary ::
                HMODULE -> IO WINBOOL
 
 foreign import ccall interruptible "GetLastError" c_GetLastError ::
@@ -66,13 +77,63 @@ foreign import ccall interruptible "GetLastError" c_GetLastError ::
 foreign import ccall interruptible "FormatMessageW"
                c_FormatMessageW ::
                DWORD ->
-                 LPCVOID -> DWORD -> DWORD -> LPWSTR -> DWORD -> Ptr () -> IO DWORD
+                 LPCVOID ->
+                   DWORD -> DWORD -> Ptr LPWSTR -> DWORD -> Ptr () -> IO DWORD
+
+foreign import ccall interruptible "LocalFree" c_LocalFree ::
+               Ptr a -> IO (Ptr a)
 
 loadModule = undefined
 
-freeModule = undefined
+freeModule m = void $ cannotbe False $ c_FreeLibrary $ getModule m
 
-findSymbol = undefined
+findSymbol m s =
+    cannotbe nullPtr $
+    withCString s $ \buf -> c_GetProcAddress (getModule m) buf
+
+cannotbe
+    :: Eq a
+    => a -> IO a -> IO a
+cannotbe v m = do
+    r <- m
+    if r == v
+        then do
+            err <- c_GetLastError
+            alloca $ \ps -> do
+                r' <-
+                    c_FormatMessageW
+                        (c_FORMAT_MESSAGE_FROM_SYSTEM .|.
+                         c_FORMAT_MESSAGE_ALLOCATE_BUFFER)
+                        nullPtr
+                        err
+                        0
+                        ps
+                        0
+                        nullPtr
+                if r' == 0
+                    then do
+                        err' <- c_GetLastError
+                        throwIO $
+                            FFIException $
+                            "Call to FormatMessageW failed with " ++
+                            show err' ++ ", last call failed with " ++ show err
+                    else bracket
+                             (peek ps)
+                             (\msgbuf -> do
+                                  msgbuf' <- c_LocalFree msgbuf
+                                  unless (msgbuf' == nullPtr) $ do
+                                      err' <- c_GetLastError
+                                      throwIO $
+                                          FFIException $
+                                          "Call to LocalFree failed with " ++
+                                          show err')
+                             (\msgbuf -> do
+                                  msg <- peekCWString msgbuf
+                                  throwIO $
+                                      FFIException $
+                                      "Last error code: " ++
+                                      show err ++ ", error message: " ++ msg)
+        else pure r
 #else
 foreign import ccall interruptible "dlopen" c_dlopen ::
                CString -> CInt -> IO (Ptr ())
